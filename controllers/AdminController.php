69c3f87476309873e5bd95f9f76276a4457ee96c
Maximiliano Vilariño <mvilarino@aldaba.es>
2026-04-07 13:05:48 +0200

    Agregar carga de configuraciones en el método actionIndex del AdminController

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -11,6 +11,7 @@ class AdminController extends Controller
     public function actionIndex()
     {
         $form = new AdminSettingsForm();
+        $form->loadSettings();
 
         if ($form->load(Yii::$app->request->post()) && $form->validate()) {
             $form->save();
